fb84247b0dbd8e95827fbb06024bf500b9baf1a6
Makhova Natalia <nataliahollt@gmail.com>
2024-06-21 15:07:24 +0300

    TW-01. Add default value for search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestsService } from "./services/requests.service";
-import { TokenResponse } from "./models/trend.model";
+import { SearchComponent } from "./components/search/search.component";
 // import { RealTimeComponent } from "./components/real-time/real-time.component";
 // import { HistoricalChartComponent } from "./components/historical-chart/historical-chart.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, SearchComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -20,8 +20,7 @@ export class AppComponent implements OnInit{
 
   ngOnInit() {
     this.requestsService.makeAuthenticatedRequest().subscribe(() => {
-      this.requestsService.getInstruments().subscribe()
+      this.requestsService.getInstruments('BTCUSD').subscribe();
     });
-
   }
 }
--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RequestsService } from "../../services/requests.service";
+import { Search } from "../../models/trend.model";
 
 @Component({
   selector: 'app-search',
@@ -8,5 +10,18 @@ import { Component } from '@angular/core';
   styleUrl: './search.component.scss'
 })
 export class SearchComponent {
+  searchResults: Search[] = [];
+  defaultSymbol = 'BTCUSD';
 
+  constructor(private requestService: RequestsService) {
+  }
+
+  onSearch(event: any) {
+    const query = event.target.value.trim();
+    const symbol = query ? query.toUpperCase() : this.defaultSymbol;
+
+    this.requestService.getInstruments(symbol).subscribe(results => {
+      this.searchResults = results;
+    });
+  }
 }
