67062f1045b6f7dde724da35fa78d56769842628
Makhova Natalia <nataliahollt@gmail.com>
2024-07-09 18:12:39 +0300

    TW-06. Add tests

--- src/app/components/real-time/real-time.component.spec.ts
+++ src/app/components/real-time/real-time.component.spec.ts
@@ -2,26 +2,80 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RealTimeComponent } from './real-time.component';
 import { HttpClientModule } from "@angular/common/http";
+import { of } from "rxjs";
+import { RequestsService } from "../../services/requests.service";
+import { RealTimeDataService } from "../../services/real-time-data.service";
+
+class MockRequestsService {
+  token = of('mockToken');
+}
+
+class MockRealTimeDataService {
+  isWebSocketNew() {
+    return true;
+  }
+  getWebSocket(token: string) {
+    return of(new WebSocket('ws://mockurl'));
+  }
+  closeWebSocket() {
+  }
+}
 
 describe('RealTimeComponent', () => {
   let component: RealTimeComponent;
   let fixture: ComponentFixture<RealTimeComponent>;
+  let requestsService: RequestsService;
+  let realTimeDataService: RealTimeDataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RealTimeComponent,
         HttpClientModule
+      ],
+      providers: [
+        { provide: RequestsService, useClass: MockRequestsService },
+        { provide: RealTimeDataService, useClass: MockRealTimeDataService }
       ]
     })
     .compileComponents();
 
     fixture = TestBed.createComponent(RealTimeComponent);
     component = fixture.componentInstance;
+    requestsService = TestBed.inject(RequestsService);
+    realTimeDataService = TestBed.inject(RealTimeDataService);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should subscribe to real-time price', () => {
+    spyOn(realTimeDataService, 'isWebSocketNew').and.returnValue(true);
+
+    component.subscribeToRealTimePrice();
+
+    expect(realTimeDataService.isWebSocketNew).toHaveBeenCalled();
+  });
+
+  it('should subscribe to real-time price and set socket', () => {
+    spyOn(realTimeDataService, 'getWebSocket').and.callThrough();
+
+    component.subscribeToRealTimePrice();
+
+    expect(realTimeDataService.getWebSocket).toHaveBeenCalledWith('mockToken');
+  });
+
+  it('should unsubscribe and close WebSocket on destroy', () => {
+    spyOn(component['destroyed$$'], 'next');
+    spyOn(component['destroyed$$'], 'complete');
+    spyOn(realTimeDataService, 'closeWebSocket');
+
+    component.ngOnDestroy();
+
+    expect(component['destroyed$$'].next).toHaveBeenCalled();
+    expect(component['destroyed$$'].complete).toHaveBeenCalled();
+    expect(realTimeDataService.closeWebSocket).toHaveBeenCalled();
   });
 });
--- src/app/components/search/search.component.spec.ts
+++ src/app/components/search/search.component.spec.ts
@@ -2,16 +2,64 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchComponent } from './search.component';
 import { HttpClientModule } from "@angular/common/http";
+import { BehaviorSubject, of } from "rxjs";
+import { RequestsService } from "../../services/requests.service";
+import { RealTimeDataService } from "../../services/real-time-data.service";
+import { ElementRef } from "@angular/core";
+import { Search } from "../../models/trend.model";
+
+class MockRequestsService {
+  private searchValue$: BehaviorSubject<string> = new BehaviorSubject<string>('');
+
+  symbol: string = '';
+  instrumentId: string = '';
+  timesArr: any[] = [];
+  pricesArr: any[] = [];
+
+  getInstruments() {
+    return of({ data: [{ symbol: 'BTCUSD', id: '1' }] });
+  }
+
+  set searchValue(val: string) {
+    this.searchValue$.next(val);
+  }
+
+  getCurrentPrice() {
+    return of({});
+  }
+
+  getHistoricalPrices() {
+    return of({});
+  }
+}
+
+class MockRealTimeDataService {
+  closeWebSocket() { }
+}
+
+class MockElementRef {
+  nativeElement = {
+    contains: (target: any) => true
+  };
+}
+
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let requestsService: RequestsService;
+  let realTimeService: RealTimeDataService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         SearchComponent,
         HttpClientModule
+      ],
+      providers: [
+        { provide: RequestsService, useClass: MockRequestsService },
+        { provide: RealTimeDataService, useClass: MockRealTimeDataService },
+        { provide: ElementRef, useClass: MockElementRef }
       ]
     })
     .compileComponents();
@@ -23,5 +71,45 @@ describe('SearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should update search results and visibility on search', () => {
+    const mockEvent = { target: { value: 'BTC' } };
+
+    component.onSearch(mockEvent);
+
+    expect(component.searchResults.length).toBe(1);
+    expect(component.filteredResults.length).toBe(1);
+    expect(component.isSearchResultsVisible).toBe(true);
+  });
+
+  it('should update request service and hide search results on data change', () => {
+    const mockResult: Search = { symbol: 'ETHUSD', id: '2', baseCurrency: "" };
+
+    component.onChangeData(mockResult);
+
+    expect(component.filteredResults.length).toBe(0);
+    expect(component.defaultSymbol).toBe('ETHUSD');
+    expect(component.isSearchResultsVisible).toBe(false);
+  });
+
+  it('should hide search results when clicking outside', () => {
+    const mockEvent = new Event('click');
+    spyOn(component["eRef"].nativeElement, 'contains').and.returnValue(false);
+
+    component.clickOutside(mockEvent);
+
+    expect(component.isSearchResultsVisible).toBe(false);
+  });
+
+  it('should unsubscribe from observables on destroy', () => {
+    spyOn(component['destroyed$$'], 'next');
+    spyOn(component['destroyed$$'], 'complete');
+
+    component.ngOnDestroy();
+
+    expect(component['destroyed$$'].next).toHaveBeenCalled();
+    expect(component['destroyed$$'].complete).toHaveBeenCalled();
   });
 });
